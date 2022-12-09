4419ecfc47e63d8dd849ce078dc72142b1d69902
PowerAngelXD <692732163@qq.com>
2022-12-10 00:20:05 +0800

    Fix: Optimize the test

--- main.cpp
+++ main.cpp
@@ -1,16 +1,16 @@
 #include "lexer/lex.h"
 
 int main() {
-    lexer::Lexer lexer = lexer::Lexer("dynic let a: String = \"hello!");
+    lexer::Lexer lexer = lexer::Lexer("dynic let a: String = \"hello!\"");
 
     try {
         lexer.generate();
+
+        for (auto tok: lexer.out) {
+            std::cout<< tok.to_string() <<std::endl;
+        }
     }
     catch (lexer_error::NotCorrectNumberError e) { e.printError(); }
     catch (lexer_error::StringUnCloseError e) { e.printError(); }
-
-    for (auto tok: lexer.out) {
-        std::cout<< tok.to_string() <<std::endl;
-    }
     return 0;
 }
