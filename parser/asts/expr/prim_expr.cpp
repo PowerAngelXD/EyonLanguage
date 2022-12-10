1efd640409a05b8d5931a4b5b85d227b5de01908
PowerAngelXD <692732163@qq.com>
2022-12-10 22:36:36 +0800

    Update: Prim Expression implement

--- parser/asts/expr/prim_expr.cpp
+++ parser/asts/expr/prim_expr.cpp
@@ -3,3 +3,17 @@
 //
 
 #include "prim_expr.h"
+
+using namespace ast;
+
+std::string PrimExprNode::to_string() {
+    return "PrimExprNode: {" + content.to_string() + "}";
+}
+
+bool PrimExprNode::is(std::vector<lexer::Token> tg, int curr_pos) {
+    return TokenNode::is(tg, curr_pos);
+}
+
+lexer::TokenKind PrimExprNode::getKind() {
+    return content.content.kind;
+}
--- parser/asts/expr/prim_expr.h
+++ parser/asts/expr/prim_expr.h
@@ -4,5 +4,24 @@
 
 #ifndef EYON_PRIM_EXPR_H
 #define EYON_PRIM_EXPR_H
+#include "../node_base.h"
+
+#define default_prim_expr_node = ast::null_prim_expr_node
+
+namespace ast {
+    /// @brief Basis of all expressions
+    struct PrimExprNode: public BasicNode {
+        TokenNode content default_token_node;
+
+        using BasicNode::BasicNode;
+
+        std::string to_string() override;
+        static bool is(std::vector<lexer::Token> tg, int curr_pos);
+
+        lexer::TokenKind getKind();
+    };
+    static PrimExprNode null_prim_expr_node = PrimExprNode(-1, -1);
+}
+
 
 #endif //EYON_PRIM_EXPR_H
