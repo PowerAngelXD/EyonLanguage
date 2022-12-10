9cf7793ef5a33dc9b1b80b9387f4767317160729
PowerAngelXD <692732163@qq.com>
2022-12-10 22:32:20 +0800

    Delete file: Delete some file

---
+++ parser/asts/expr/prim_expr.cpp
@@ -0,0 +1,5 @@
+//
+// Created by BALL on 2022/12/10.
+//
+
+#include "prim_expr.h"
---
+++ parser/asts/expr/prim_expr.h
@@ -0,0 +1,8 @@
+//
+// Created by BALL on 2022/12/10.
+//
+
+#ifndef EYON_PRIM_EXPR_H
+#define EYON_PRIM_EXPR_H
+
+#endif //EYON_PRIM_EXPR_H
