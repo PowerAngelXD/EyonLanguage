110431d04c13fc418b30a7395ad8917f6933bf8b
PowerAngelXD <692732163@qq.com>
2022-12-11 22:15:02 +0800

    New file: parser file

---
+++ parser/parser.h
@@ -0,0 +1,15 @@
+//
+// Created by BALL on 2022/12/11
+//
+#ifndef EYON_PARSER_H
+#define EYON_PARSER_H
+
+#include "asts/ast.h"
+
+namespace parser {
+    class Parser {
+        std::vector<lexer::Token> tg;
+    };
+}
+
+#endif // !EYON_PARSER_H
