c4ce258340821341dfcbf712aa39af508c387a59
PowerAngelXD <692732163@qq.com>
2022-12-11 13:09:49 +0800

    New files: Add expression implement

---
+++ parser/asts/expr/add_expr.cpp
@@ -0,0 +1,21 @@
+//
+// Created by BALL on 2022/12/11
+//
+#include "add_expr.h"
+
+using namespace ast;
+
+std::string AddExprNode::to_string() {
+    std::string ret = "AddExprNode: {[" + head.to_string() + ", ";
+    for(int i = 0; i < ops.size(); i ++) {
+        ret += ops[i].to_string() + ", ";
+        ret += factors[i].to_string() + ", ";
+    }
+    ret.erase(ret.end() - 1, ret.end());
+    ret += "]}";
+    return ret;
+}
+
+bool AddExprNode::is(std::vector<lexer::Token> tg, int curr_pos) {
+    return MulExprNode::is(tg, curr_pos);
+}
---
+++ parser/asts/expr/add_expr.h
@@ -0,0 +1,25 @@
+//
+// Created by BALL on 2022/12/11
+//
+#ifndef EYON_ADD_EXPR_h
+#define EYON_ADD_EXPR_H
+
+#include "mul_expr.h"
+
+#define default_add_expr_node = null_add_expr_node
+
+namespace ast {
+    struct AddExprNode: public BasicNode {
+        MulExprNode head default_mul_expr_node;
+        std::vector<MulExprNode> factors;
+        std::vector<AddOpNode> ops;
+
+        using BasicNode::BasicNode;
+
+        std::string to_string();
+        static bool is(std::vector<lexer::Token> tg, int curr_pos);    
+    };
+    static AddExprNode null_add_expr_node = AddExprNode(-1, -1);
+}
+
+#endif // !EYON_ADD_EXPR_h
