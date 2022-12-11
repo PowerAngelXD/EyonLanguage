79f5650cf8591bdde518cba411d1800f85b713b2
PowerAngelXD <692732163@qq.com>
2022-12-11 13:10:32 +0800

    Update: update a default value for MulExpr

--- parser/asts/expr/mul_expr.h
+++ parser/asts/expr/mul_expr.h
@@ -2,8 +2,13 @@
 // Created by BALL on 2022/12/10
 //
 #ifndef EYON_MUL_EXPR_H
+#define EYON_MUL_EXPR_H
+
 #include "prim_expr.h"
+#include "op/add_op.h"
 #include "op/mul_op.h"
+
+#define default_mul_expr_node = null_mul_expr_node
 
 namespace ast { 
     struct MulExprNode: public BasicNode {
@@ -16,6 +21,7 @@ namespace ast {
         std::string to_string() override;
         static bool is(std::vector<lexer::Token> tg, int curr_pos);
     };
+    static MulExprNode null_mul_expr_node = MulExprNode(-1, -1);
 }
 
 #endif // !EYON_MUL_EXPR_H
