dbb4483e5a552741da51a661f71c76efa2d734ab
PowerAngelXD <692732163@qq.com>
2022-12-13 16:44:10 +0800

    Update: Test ast

--- main.cpp
+++ main.cpp
@@ -1,14 +1,15 @@
 #include "lexer/lex.h"
+#include "parser/parsers/expr/expr_parser.h"
 
 int main() {
-    lexer::Lexer lexer = lexer::Lexer("dynic let a: String = \"hello!\"");
+    lexer::Lexer lexer = lexer::Lexer("1+2");
 
     try {
         lexer.generate();
-
-        for (auto tok: lexer.out) {
-            std::cout<< tok.to_string() <<std::endl;
-        }
+        parser::AddExprParser addexpr(lexer.out);
+        addexpr.parse();
+        auto a=1;
+        std::cout<<a<<std::endl;
     }
     catch (lexer_error::NotCorrectNumberError& e) { e.printError(); }
     catch (lexer_error::StringUnCloseError& e) { e.printError(); }
